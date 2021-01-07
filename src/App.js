51dbef15cd03619fad242c0cd020dbe0aff7636c
Paul Gates <pgates@paychex.com>
2021-01-07 13:42:13 -0500

    Initial Commit of Monsters Rolodex pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      searchField: '',
+       monsters: [
+      //   {
+      //     name: 'Frankenberry',
+      //     id: 'm1'
+      //   },
+      //   {
+      //     name: 'Count Chocula',
+      //     id: 'm2'
+      //   },
+      //   {
+      //     name: 'Boo Berry',
+      //     id: 'm3'
+      //   }
+      ]
+    };
+
+    // Use arrow functions instead of having to declare this here
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(userData => console.log(this.setState({ monsters: userData })));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+    const { searchField, monsters } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder='search monsters' 
+                   handleChange={this.handleChange}/>
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
